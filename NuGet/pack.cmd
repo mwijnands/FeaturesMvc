49f36c330c303a1d7dcc2384b50501a12c2218e5
Marcel Wijnands <marcel@shynet.nl>
2014-11-08 00:15:18 +0100

    Added NuGet stuff and docs image.

---
+++ NuGet/pack.cmd
@@ -0,0 +1,2 @@
+nuget pack ../FeaturesMvc/FeaturesMvc.csproj -Build -Prop Configuration=Release
+nuget pack ../FeaturesMvc.Bundling/FeaturesMvc.Bundling.csproj -Build -IncludeReferencedProjects -Prop Configuration=Release
